b104e15d0350abc049ea0c4cef5720740d7d7fc2
Arpan Sarkar <arpansarkar112@gmail.com>
2026-07-02 19:38:42 +0300

    feat: changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from 'nitro/vite';
 
 export default defineConfig({
-  plugins: [tanstackStart({ server: { entry: "server" } }), react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tanstackStart({ server: { entry: "server" } }), react(), tailwindcss(), tsconfigPaths(), nitro()],
 });
